22fa558ca1cb1b2c56584b7952eaee2744b9efaf
uptheshipcreek <northenore@gmail.com>
2023-11-02 02:09:54 +0200

    Added the key functions and updated gitignore

--- samples/Pools/script.cpp
+++ samples/Pools/script.cpp
@@ -1,9 +1,18 @@
 #include "script.h"
 #include <string>
 #include <vector>
+#include <chrono>
+#include <thread>
 
-void update(){
-	PLAYER::SET_PLAYER_INVINCIBLE(PLAYER::PLAYER_ID(), true);
+void PRINT_MESSAGE(const char* message) {
+	UILOG::_UILOG_SET_CACHED_OBJECTIVE(message);
+	UILOG::_UILOG_PRINT_CACHED_OBJECTIVE();
+	UILOG::_UILOG_CLEAR_HAS_DISPLAYED_CACHED_OBJECTIVE();
+	UILOG::_UILOG_CLEAR_CACHED_OBJECTIVE();
+}
+
+void update() {
+	PRINT_MESSAGE("Can you see this?");
 }
 
 void main()
