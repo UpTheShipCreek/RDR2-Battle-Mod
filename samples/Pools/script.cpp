f2d3222c2ad302f7bd4a2c5531d586297e90f2ed
uptheshipcreek <northenore@gmail.com>
2023-11-03 23:42:25 +0200

    Okay got to print a statement when pressign a key

--- samples/Pools/script.cpp
+++ samples/Pools/script.cpp
@@ -1,18 +1,26 @@
 #include "script.h"
+
 #include <string>
 #include <vector>
 #include <chrono>
 #include <thread>
+#include <Windows.h>
 
-void PRINT_MESSAGE(const char* message) {
+void print_message(const char* message) {
 	UILOG::_UILOG_SET_CACHED_OBJECTIVE(message);
 	UILOG::_UILOG_PRINT_CACHED_OBJECTIVE();
 	UILOG::_UILOG_CLEAR_HAS_DISPLAYED_CACHED_OBJECTIVE();
 	UILOG::_UILOG_CLEAR_CACHED_OBJECTIVE();
 }
 
-void update() {
-	PRINT_MESSAGE("Can you see this?");
+bool is_key_pressed(int key) {
+	return GetAsyncKeyState(key) & 0x8000;
+}
+
+void update(){
+	if (is_key_pressed(VK_F9)) {
+		print_message("Can you see this?");
+	}
 }
 
 void main()
